788d0c28f366280c8b264d118a74bf839e766d29
Krendelev <krendelev@gmail.com>
2021-07-23 16:13:00 +0300

    make view respose with JSON

--- places/urls.py
+++ places/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import index
+from .views import get_places, get_place_details
 
 urlpatterns = [
-    path("", index),
+    path("", get_places, name="places"),
+    path("places/<int:pk>", get_place_details, name="place-details"),
 ]
--- places/views.py
+++ places/views.py
@@ -1,30 +1,47 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+
+from .models import Place
 
 
-def index(request):
-    places = {
-        "type": "FeatureCollection",
-        "features": [
-            {
-                "type": "Feature",
-                "geometry": {"type": "Point", "coordinates": [37.62, 55.793676]},
-                "properties": {
-                    "title": "«Легенды Москвы",
-                    "placeId": "moscow_legends",
-                    "detailsUrl": "./static/places/moscow_legends.json",
-                },
-            },
-            {
-                "type": "Feature",
-                "geometry": {"type": "Point", "coordinates": [37.64, 55.753676]},
-                "properties": {
-                    "title": "Крыши24.рф",
-                    "placeId": "roofs24",
-                    "detailsUrl": "./static/places/roofs24.json",
-                },
-            },
-        ],
+def get_places_data(places):
+    places_data = {
+        "places": {
+            "type": "FeatureCollection",
+            "features": [
+                {
+                    "type": "Feature",
+                    "geometry": {
+                        "type": "Point",
+                        "coordinates": [place.longitude, place.latitude],
+                    },
+                    "properties": {
+                        "title": place.title,
+                        "placeId": place.id,
+                        "detailsUrl": reverse("place-details", args=[place.id]),
+                    },
+                }
+                for place in places
+            ],
+        }
     }
-    context = {"places": places}
-    
+    return places_data
+
+
+def get_places(request):
+    places = Place.objects.all()
+    context = get_places_data(places)
     return render(request, "index.html", context=context)
+
+
+def get_place_details(request, pk):
+    place = get_object_or_404(Place, pk=pk)
+    context = {
+        "title": place.title,
+        "imgs": [obj.photo.url for obj in place.photos.all()],
+        "description_short": place.short_description,
+        "description_long": place.long_description,
+        "coordinates": {"lat": place.latitude, "lng": place.longitude},
+    }
+    return JsonResponse(context, json_dumps_params={"ensure_ascii": False, "indent": 2})
